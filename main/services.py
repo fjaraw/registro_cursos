3b1331e5cdade2366b49dea1ffb9eb0b221470d9
Fernando Jara <fjaraw@hotmail.com>
2024-06-28 22:25:23 -0400

    Funciones de obtener información de estudiantes, profesores y cursos.

--- main/services.py
+++ main/services.py
@@ -1,4 +1,4 @@
-from main.models import Curso, Estudiante, Profesor
+from main.models import Curso, Direccion, Estudiante, Profesor
 
 def crear_curso(codigo,nombre,version):
     #p = Profesor.objects.get(rut=profesor_rut)
@@ -13,14 +13,20 @@ def crear_estudiante(rut,nombre,apellido,fecha_nacimiento):
     e = Estudiante(rut=rut,nombre=nombre,apellido=apellido,fecha_nacimiento=fecha_nacimiento,activo=True)
     e.save()
     return e
-def crear_direccion():
-    pass
-def obtiene_estudiante():
-    pass
-def obtiene_profesor():
-    pass
-def obtiene_curso():
-    pass
+def crear_direccion(calle,numero,comuna,region,estudiante_rut):
+    e = Estudiante.objects.get(rut=estudiante_rut)
+    d = Direccion(calle=calle,numero=numero,comuna=comuna,region=region,estudiante=e)
+    d.save()
+    return d
+def obtiene_estudiante(estudiante_rut):
+    e = Estudiante.objects.get(rut=estudiante_rut)
+    return e
+def obtiene_profesor(profesor_rut):
+    p = Profesor.objects.get(rut=profesor_rut)
+    return p
+def obtiene_curso(curso_codigo):
+    c = Curso.objects.get(codigo=curso_codigo)
+    return c
 def agrega_profesor_a_curso(profesor_rut, curso_codigo):
     p = Profesor.objects.get(rut=profesor_rut)
     c = Curso.objects.get(codigo=curso_codigo)
@@ -30,5 +36,9 @@ def agrega_cursos_a_estudiante(curso_codigo, estudiante_rut):
     c = Curso.objects.get(codigo=curso_codigo)
     e = Estudiante.objects.get(rut=estudiante_rut)
     c.estudiantes.add(e)
-def imprime_estudiante_cursos():
-    pass
+def imprime_estudiante_cursos(estudiante_rut):
+    estudiante = obtiene_estudiante(estudiante_rut)
+    print(f'Cursos del estudiante {estudiante.nombre} {estudiante.apellido}')
+    cursos = estudiante.cursos.all()
+    for curso in cursos:
+        print(f'    {curso.nombre}')
